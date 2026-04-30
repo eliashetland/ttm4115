a5b7c4b41db1668070075a8a559f90e6e9d5d130
Xmaster12 <72987131+Xmaster12@users.noreply.github.com>
2026-04-30 23:45:39 +0200

    Update heartbeatController.ts

--- backend/src/controllers/heartbeatController.ts
+++ backend/src/controllers/heartbeatController.ts
@@ -3,44 +3,47 @@ import type { IDronePosition } from "../models/dronePositionModel.js";
 
 
 
-export const updateDroneFromHeartbeat = (droneId: number, batteryLevel: number, dronePosition: IDronePosition) => {
+
+export const updateDroneFromHeartbeat = (
+    droneId: number,
+    batteryLevel: number,
+    dronePosition: IDronePosition,
+    deliveryStatus: string
+) => {
 
     if (!validateHeartbeatData(droneId, batteryLevel, dronePosition)) {
         return null;
     }
 
     const drone = drones.find(drone => drone.droneId === droneId);
-
     if (!drone) {
         return null;
     }
-
 
     if (drone.orderId) {
         const order = orders.find(order => order.id === drone.orderId);
         if (order) {
             order.history.push({
                 createdAt: new Date(dronePosition.timestamp),
-                status: "In-Flight",
+                status: deliveryStatus,
                 location: {
                     latitude: dronePosition.latitude,
                     longitude: dronePosition.longitude,
                     description: "In transit"
                 },
                 type: "drone",
-                message: `Drone ${drone.name} is in-flight with the order`
+                message: `Drone ${drone.name} status: ${deliveryStatus}`
             });
         }
     }
 
     drone.batteryLevel = batteryLevel;
     drone.position = dronePosition;
-    
+    drone.deliveryStatus = deliveryStatus;
 
     return drone;
+};
 
-
-}
 
 const validateHeartbeatData = (droneId: number, batteryLevel: number, dronePosition: IDronePosition) => {
     if (!droneId || batteryLevel < 0 || batteryLevel > 100) {
